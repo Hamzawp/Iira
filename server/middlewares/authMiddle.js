e8b8d3f83d4008c898607ae4f69e3c9703c7c4da
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-09-12 17:27:25 +0530

    dfoshfoishfoshdfasdhfoish

--- server/middlewares/authMiddle.js
+++ server/middlewares/authMiddle.js
@@ -30,3 +30,32 @@ exports.checkForSPOC = async (req, res, next) => {
       .json({ error: "Internal Server Error", message: error.message });
   }
 };
+
+exports.checkForCollegeFaculty = async (req, res, next) => {
+  try {
+    // console.log(req.headers);
+    const token = req.headers.authtoken;
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(payload);
+    const spoc = await prisma.user.findUnique({
+      where: { user_id: payload.id, role: "college_faculty" },
+      // where: { user_id: payload.id },
+    });
+    // console.log(spoc);
+    if (!spoc) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    req.user = spoc;
+    next();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+  }
+};
