71703615e1847e3ff443b9e0b9eb1c9a371fac84
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-09-12 18:38:42 +0530

    andi bandi sandi

--- server/middlewares/authMiddle.js
+++ server/middlewares/authMiddle.js
@@ -59,3 +59,37 @@ exports.checkForCollegeFaculty = async (req, res, next) => {
       .json({ error: "Internal Server Error", message: error.message });
   }
 };
+
+exports.checkForUniversityFaculty = async (req, res, next) => {
+  try {
+    // console.log(req.headers);
+    const token = req.headers.authtoken;
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(payload);
+    const university_faculty = await prisma.user.findUnique({
+      where: { user_id: payload.id, role: "university" },
+      // where: { user_id: payload.id },
+    });
+    // console.log(spoc);
+    if (!university_faculty) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    req.user = university_faculty;
+    const university = await prisma.Universities.findUnique({
+      where: { uni_head_id: university_faculty.user_id },
+    });
+    req.user.university_id = university.uni_id;
+    // console.log(req.user);
+    next();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+  }
+};
