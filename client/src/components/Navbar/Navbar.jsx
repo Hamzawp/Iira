a7b5ffbae320772cf4ce0334c972c85aa9ae5493
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-09-25 23:15:31 +0530

    push

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { AiOutlineBell, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import {BiMenu} from 'react-icons/bi'
-import Avatar from 'react-avatar';
-import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
+import { BiMenu } from "react-icons/bi";
+import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
+import { FormControl, InputLabel, Select } from "@mui/material";
+import MaterialMenuItem from "@mui/material/MenuItem";
+import i18next from "i18next";
+
 export default function Navbar(props) {
   const [turn, setTurn] = useState(false);
   const handleSwitch = () => {
@@ -16,24 +19,49 @@ export default function Navbar(props) {
     }
   };
 
+  const [age, setAge] = React.useState("en");
+
+  const handleChange = (event) => {
+    i18next.changeLanguage(event.target.value);
+    setAge(event.target.value);
+  };
+
   return (
     //  NAVBAR
     <nav>
       {/* <i className="bx bx-menu"></i> */}
-      <BiMenu
-        onClick={props.onClick}
-        className="nav-link"
-        id="hammenu"
-      />
+      <BiMenu onClick={props.onClick} className="nav-link" id="hammenu" />
       {/* <a href="#" className="nav-link">Categories</a>  */}
       <form action="#">
         <div className="form-input">
           <input type="search" placeholder="Search..." />
           <button type="submit" className="search-btn">
-          <AiOutlineSearch/>
+            <AiOutlineSearch />
           </button>
         </div>
       </form>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Lang</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={age}
+          onChange={handleChange}
+          label="Lang"
+        >
+          <MaterialMenuItem value={"en"}>English</MaterialMenuItem>
+          <MaterialMenuItem value={"hi"}>Hindi</MaterialMenuItem>
+          <MaterialMenuItem value={"ta"}>Tamil</MaterialMenuItem>
+          <MaterialMenuItem value={"te"}>Telugu</MaterialMenuItem>
+          <MaterialMenuItem value={"kn"}>Kannada</MaterialMenuItem>
+          <MaterialMenuItem value={"ml"}>Malayalam</MaterialMenuItem>
+          <MaterialMenuItem value={"mr"}>Marathi</MaterialMenuItem>
+          <MaterialMenuItem value={"gu"}>Gujarati</MaterialMenuItem>
+          <MaterialMenuItem value={"bn"}>Bengali</MaterialMenuItem>
+          <MaterialMenuItem value={"pa"}>Punjabi</MaterialMenuItem>
+          <MaterialMenuItem value={"or"}>Odia</MaterialMenuItem>
+        </Select>
+      </FormControl>
       <input type="checkbox" onChange={handleSwitch} id="switch-mode" hidden />
       <label for="switch-mode" className="switch-mode"></label>
       <a href="#" className="notification">
@@ -42,10 +70,11 @@ export default function Navbar(props) {
         <span className="num">5</span>
       </a>
       <a href="#" className="profile">
-      
-      <Menu menuButton={<MenuButton style={{"border":"none"}}><Avatar src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3" size="30" round={true} /></MenuButton>}>
-      <MenuItem>Logout</MenuItem>
-    </Menu>
+        <Menu
+          menuButton={<MenuButton style={{ border: "none" }}>hello</MenuButton>}
+        >
+          <MenuItem>Logout</MenuItem>
+        </Menu>
       </a>
     </nav>
     //    NAVBAR
